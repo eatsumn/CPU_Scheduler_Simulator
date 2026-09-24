b9f3a6a1a94434d5418ab86f8f4eacf91cecaf31
eats <92004049+eatsumn@users.noreply.github.com>
2026-09-25 03:57:54 +0800

    Prototype Menu

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module org.lorelei.cpu_scheduler {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
 
 
     opens org.lorelei.cpu_scheduler to javafx.fxml;
--- src/main/java/org/lorelei/cpu_scheduler/Main.java
+++ src/main/java/org/lorelei/cpu_scheduler/Main.java
@@ -1,4 +1,22 @@
 package org.lorelei.cpu_scheduler;
 
-public class Main {
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args){ launch(args);}
+
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.setResizable(false);
+        stage.show();
+    }
 }
